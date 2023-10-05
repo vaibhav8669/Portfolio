de6dff194e7d91776887c28ae25fc8af5032469c
Vaibhav Bergal <112370382+vaibhav8669@users.noreply.github.com>
2023-10-05 13:57:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ form.addEventListener("submit", (e) => {
       msg.innerHTML = "Message sent successfully";
       setTimeout(function () {
         msg.innerHTML = "";
-      }, 2000);
+      }, 5000);
       form.reset();
     })
     .catch((error) => console.error("Error!", error.message));
